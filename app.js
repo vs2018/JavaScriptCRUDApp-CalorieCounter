35a98cec3eaafe75f5585466bbc9669cb91fa1e2
Vishal Sharma <vsh1819@gmail.com>
2018-04-09 16:14:03 +0100

    added localstorage to persist the items

--- app.js
+++ app.js
@@ -1,5 +1,48 @@
 // Storage Controller
 
+const StorageCtrl = (function(){
+  return {
+    storeItem: function(item){
+      let items
+
+      // Check if any items in ls
+      if(localStorage.getItem('items') === null){
+        items = []
+        // Push new item
+        items.push(item)
+
+        // Set ls
+
+        localStorage.setItem('items', JSON.stringify(items))
+      } else {
+        // Get what is already in ls
+        items = JSON.parse(localStorage.getItem('items'))
+
+        // Push new item 
+        items.push(item)
+
+        // Reset ls
+        localStorage.setItem('items', JSON.stringify(items))
+
+      }
+
+    },
+
+    getItemsFromStorage: function(){
+      let items
+      if(localStorage.getItem('items') === null){
+        items = []
+
+      } else {
+        items = JSON.parse(localStorage.getItem('items'))
+
+      }
+
+      return items
+    }
+  }
+})()
+
 // Item Controller
 const ItemCtrl = (function(){
   // Item Constructor
@@ -11,11 +54,12 @@ const ItemCtrl = (function(){
 
   // Data Structure / State
   const data = {
-    items: [
-      // {id: 0, name: 'Steak Dinner', calories: 1200},
-      // {id: 1, name: 'Cookie', calories: 400},
-      // {id: 2, name: 'Eggs', calories: 300}
-    ],
+    // items: [
+    //   // {id: 0, name: 'Steak Dinner', calories: 1200},
+    //   // {id: 1, name: 'Cookie', calories: 400},
+    //   // {id: 2, name: 'Eggs', calories: 300}
+    // ],
+    items: StorageCtrl.getItemsFromStorage(),
     currentItem: null,
     totalCalories: 0
   }
@@ -255,7 +299,7 @@ const UICtrl = (function(){
 })()
 
 // App Controller
-const App = (function(ItemCtrl, UICtrl){
+const App = (function(ItemCtrl, StorageCtrl, UICtrl){
 
 
   // Load event listeners
@@ -313,6 +357,9 @@ const App = (function(ItemCtrl, UICtrl){
 
       // Add total calories to UI
       UICtrl.showTotalCalories(totalCalories)
+
+      // Store in localStorage 
+      StorageCtrl.storeItem(newItem)
 
       // Clear fields
       UICtrl.clearInput()
@@ -463,7 +510,7 @@ const App = (function(ItemCtrl, UICtrl){
   }
   
   
-})(ItemCtrl, UICtrl)
+})(ItemCtrl, StorageCtrl, UICtrl)
 
 // Initialise App
 App.init()

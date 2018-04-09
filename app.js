3afc2af1a2d47e7c9c0c519633ada312e39982f2
Vishal Sharma <vsh1819@gmail.com>
2018-04-09 13:58:43 +0100

    cleared input fields once meal added and also hide UL if there are no items in the list

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ const ItemCtrl = (function(){
   // Data Structure / State
   const data = {
     items: [
-      {id: 0, name: 'Steak Dinner', calories: 1200},
-      {id: 1, name: 'Cookie', calories: 400},
-      {id: 2, name: 'Eggs', calories: 300}
+      // {id: 0, name: 'Steak Dinner', calories: 1200},
+      // {id: 1, name: 'Cookie', calories: 400},
+      // {id: 2, name: 'Eggs', calories: 300}
     ],
     currentItem: null,
     totalCalories: 0
@@ -94,6 +94,9 @@ const UICtrl = (function(){
         calories: document.querySelector(UISelectors.itemCaloriesInput).value
       }
     },addListItem: function(item){
+      // Show the list
+      document.querySelector(UISelectors.itemList).style.display = 'block'
+
       // Create li element
       const li = document.createElement('li')
       // Add class
@@ -108,6 +111,13 @@ const UICtrl = (function(){
       </a>`
       // Insert item
       document.querySelector(UISelectors.itemList).insertAdjacentElement('beforeend', li)
+    },
+    clearInput: function(){
+      document.querySelector(UISelectors.itemNameInput).value = ''
+      document.querySelector(UISelectors.itemCaloriesInput).value = ''
+    },
+    hideList: function(){
+      document.querySelector(UISelectors.itemList).style.display = 'none'
     },
     getSelectors: function(){
       return UISelectors
@@ -144,6 +154,9 @@ const App = (function(ItemCtrl, UICtrl){
       // Add item to UI list
 
       UICtrl.addListItem(newItem)
+
+      // Clear fields
+      UICtrl.clearInput()
       
     }
     
@@ -159,8 +172,15 @@ const App = (function(ItemCtrl, UICtrl){
       // Fetch items from data structure 
       const items = ItemCtrl.getItems()
 
+      // Check if any items
+      if(items.length === 0){
+        UICtrl.hideList()
+      } else {
+
       // Populate list with items
       UICtrl.populateItemList(items)
+      }
+
 
       // Load event listeners
       loadEventListeners()
